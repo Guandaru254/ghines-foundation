17fd69859fc02c8b519b6fbc734ca332adde74cb
Guandaru254 <leroykim82@gmail.com>
2025-09-10 14:42:18 +0300

    Favicon updated

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          {/* Favicon link */}
+          <link rel="icon" href="/images/favicon.png" type="image/png" />
+
+          {/* Your existing Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
@@ -14,6 +19,8 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
+
+export default MyDocument;
